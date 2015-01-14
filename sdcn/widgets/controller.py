df88d736a645d7ae80b437d7c2258238442befaf
taewankung <taewankung@megarock>
2015-01-14 16:55:39 +0700

    up cotroller

--- sdcn/widgets/controller.py
+++ sdcn/widgets/controller.py
@@ -3,6 +3,10 @@ Created on Jan 14, 2015
 
 @author: taewankung
 '''
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+import os
 
-class SdcnController:
+Builder.load_file(os.path.dirname(__file__) + '/controller.kv')
+class SdcnController(BoxLayout):
     pass
