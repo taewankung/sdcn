7fe62a8a73060e5e757f4ea78a65587c36701d09
taewankung <taewankung@megarock>
2015-02-11 19:14:39 +0700

    add save pop up

--- sdcn/widgets/controller.py
+++ sdcn/widgets/controller.py
@@ -16,10 +16,11 @@ from kivy.uix.filechooser import FileChooserListView
 from kivy.uix.button import Button
 import os
 from kivy.uix.popup import Popup
-import subprocess
+
 from sdcn import commands
 from kivy.core.audio import SoundLoader 
 from kivy.uix.label import Label
+from sdcn.widgets.save_open.save_file import SavePopup
 
 import json
 from io import StringIO
@@ -32,6 +33,7 @@ Builder.load_file(os.path.dirname(__file__) + '/controller.kv')
 class SdcnController(FloatLayout):
     complete = Popup(title = 'Complete',size_hint=(0.5,0.5))
     complete.add_widget(Label(text = 'Complete'))
+    
     def __init__(self):
         super().__init__()
 #         self.pdf_menu = PdfMenu()
@@ -43,13 +45,12 @@ class SdcnController(FloatLayout):
         self.main_menu_layout = self.ids.main_menu_layout
         self.sub_menu_layout = self.ids.sub_menu_layout
         self.workflow_layout = self.ids.workflow_layout
-        
         self.submenus = [PdfMenu(self.workflow_layout, self), DocumentMenu(self.workflow_layout, self), 
                          FileAndFolderMenu(self.workflow_layout, self), ImageMenu(self.workflow_layout, self), 
                          MusicMenu(self.workflow_layout, self), VideoMenu(self.workflow_layout, self)]
-        
-        
+        self.save_popup = SavePopup(title = 'Save',workflow_layout = self.workflow_layout)
         self.workflow_layout.bind(minimum_height=self.workflow_layout.setter('height'))
+   
     def status_play_button(self):
         self.ids.play_button.enable += 1
         if self.ids.play_button.enable % 2 == 0:
@@ -72,7 +73,7 @@ class SdcnController(FloatLayout):
 #                     test1 = ['find', str(path_file) ,  '-name',str(bt.widget.ids.file_input.text)]
 #                     test = subprocess.check_output(test1)
 #                     print(test)
-                    print(type(selection_file))
+                    print(type(selection_fisave_popuple))
 #                     cmd = commands.FindCommand(path=path_file, pattern = str(selection_file[0]))
                     
                     cmd = commands.FindCommand(path=path_file, pattern = bt.widget.ids.file_input.text)
@@ -217,28 +218,29 @@ class SdcnController(FloatLayout):
                 break
             
     def save_workflow(self):
-        workflow=[]
-
-        for wf_widget in reversed(self.workflow_layout.children):
-            wf = dict()
-            if wf_widget.widget.ids.workflow_header.text == 'Find File':
-                wf = dict(name=wf_widget.widget.ids.workflow_header.text ,
-                          path_file = wf_widget.widget.ids.path_input.text,
-                          pattern = wf_widget.widget.ids.file_input.text
-                        )
-            elif wf_widget.widget.ids.workflow_header.text == 'Convert PDF File':
-                wf = dict(name=wf_widget.widget.ids.workflow_header.text ,
-                          type = wf_widget.widget.ids.type.text,
-                          target = wf_widget.widget.ids.nameinput.text
-                        )
-            workflow.append(wf)
-
-        workflow_file = dict(workflow=workflow)
-        
-        with open('/tmp/work_flow.json', 'w') as f:
-            json.dump(workflow_file, f)
-        
-        f.close()
+        self.save_popup.open()
+#         workflow=[]
+# 
+#         for wf_widget in reversed(self.workflow_layout.children):
+#             wf = dict()
+#             if wf_widget.widget.ids.workflow_header.text == 'Find File':
+#                 wf = dict(name=wf_widget.widget.ids.workflow_header.text ,
+#                           path_file = wf_widget.widget.ids.path_input.text,
+#                           pattern = wf_widget.widget.ids.file_input.text
+#                         )
+#             elif wf_widget.widget.ids.workflow_header.text == 'Convert PDF File':
+#                 wf = dict(name=wf_widget.widget.ids.workflow_header.text ,
+#                           type = wf_widget.widget.ids.type.text,
+#                           target = wf_widget.widget.ids.nameinput.text
+#                         )
+#             workflow.append(wf)
+# 
+#         workflow_file = dict(workflow=workflow)
+#          
+#         with open('/tmp/work_flow.json', 'w') as f:
+#             json.dump(workflow_file, f)
+#          
+#         f.close()
 
     def open_workflow(self):
         workflow_file = None
@@ -254,23 +256,9 @@ class SdcnController(FloatLayout):
                 widget = dw.widget
                 widget.ids.path_input.text = wf_dict['path_file']
                 widget.ids.file_input.text = wf_dict['pattern']
-            elif wf_dict['name'] == "Convert PDF file":
+            elif wf_dict['name'] == "Convert PDF File":
                 dw = submenu.add_widget_to_workflow_layout(wf_dict['name'])
                 widget = dw.widget
                 widget.ids.type.text = wf_dict['type']
                 widget.ids.nameinput.text = wf_dict['target']
 
-
-    def save_as(self):
-        filechoser_layout = StackLayout( orientation="lr-bt")
-        filechoser = FileChooserListView( size_hint = (0.75,1), size=(1,400))
-        filechoser_layout.add_widget(filechoser)
-        ok_button = Button(text = 'Ok' , size_hint = (0.12,None), size=(1,25))
-        cancel_button = Button(text = 'Cancel' ,size_hint = (0.12,None), size=(1,25))
-        filechoser_layout.add_widget(ok_button)
-        filechoser_layout.add_widget(cancel_button)
-        popup_browser = Popup(title = 'Save As...')
-        popup_browser.add_widget(filechoser_layout)
-        cancel_button.bind(on_press = popup_browser.dismiss)
-        popup_browser.open()
-        pass
